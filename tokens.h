a095b6aa23416f931c4a4335259367f805c2c80d
artn-dev <azemarteixeira@gmail.com>
2021-09-11 22:33:20 -0300

    add struct HkToken_t

--- tokens.h
+++ tokens.h
@@ -8,4 +8,27 @@ enum HkTokenType {
         HK_WHITESPACE,
 };
 
+/**
+ * Represents a token.
+ * The token assigns a group of characters read from a file to a type of
+ * object recognizable by the parser.
+ */
+struct HkToken_t {
+        /**
+         * The token's type.
+         * A object type recognizable by the parser.
+         * @see HkTokenType
+         */
+        enum HkTokenType type;
+
+        /**
+         * The token's value.
+         * A group of characters read from a file.
+         */
+        char *data;
+};
+
+typedef struct HkToken_t HkToken;
+
+
 #endif /* HACK_TOKENS_H */
