f7408afa437fac5463ac8cc1d0ea2214b245331c
artn-dev <azemarteixeira@gmail.com>
2021-09-11 22:11:06 -0300

    add enum HkTokenType

---
+++ tokens.h
@@ -0,0 +1,11 @@
+#ifndef HACK_TOKENS_H
+#define HACK_TOKENS_H
+
+/**
+ * Represents the differents types of tokens.
+ */
+enum HkTokenType {
+        HK_WHITESPACE,
+};
+
+#endif /* HACK_TOKENS_H */
