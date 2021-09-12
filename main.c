4d5859a9c361843b7907a9c59f72479b99b24e32
artn-dev <azemarteixeira@gmail.com>
2021-09-11 22:00:37 -0300

    add function getEOF

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+long int getEOF(FILE* file)
+{
+	fseek(file, 0, SEEK_END);
+	long int eof = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	return eof;
+}
+
 int main(int argc, char** argv)
 {
 	printf("Hello, World!\n");
