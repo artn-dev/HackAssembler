6f778302d5eb422a0b202b759b363bb1845d21fe
artn-dev <azemarteixeira@gmail.com>
2021-10-02 01:11:18 -0300

    only parse identifiers as literals

--- src/parser.c
+++ src/parser.c
@@ -3,7 +3,9 @@
 
 _HkTokenListNode *hkParseLiteral(_HkTokenListNode *token, HkStatement *statement)
 {
-        statement->type = HK_ST_LITERAL;
-        statement->value = token->data.data;
+        if (token->data.type == HK_TK_IDENTIFIER) {
+                statement->type = HK_ST_LITERAL;
+                statement->value = token->data.data;
+        }
         return token->next;
 }
