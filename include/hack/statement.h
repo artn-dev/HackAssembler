4624fc98abaa8dd85b2726a94a642e4bf516a25b
artn-dev <azemarteixeira@gmail.com>
2021-10-02 01:12:00 -0300

    add statement type for binary operations

--- include/hack/statement.h
+++ include/hack/statement.h
@@ -7,6 +7,7 @@
  */
 enum HkStatementType {
         HK_ST_LITERAL,
+        HK_ST_BIN_OP,
 };
 
 /**
@@ -20,7 +21,17 @@ typedef struct HkStatement {
          * @see HkStatementType
          */
         enum HkStatementType type;       
-        char *value;
+
+        /**
+         * Represents the data related to different statements.
+         */
+        union{
+                char *value;
+                struct {
+                        struct HkStatement *params;
+                        size_t paramCount;
+                };
+        };
 } HkStatement;
 
 #endif /* HACK_STATEMENT_H */
